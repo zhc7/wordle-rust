9e5da22de8daa5e688da6cdd1401da20265b237c
zhc7 <hc-zhang22@mails.tsinghua.edu.cn>
2023-07-03 00:21:32 +0800

    fix(args): change difficult -d into -D and improve args style

--- src/main.rs
+++ src/main.rs
@@ -12,26 +12,27 @@ use rand::seq::SliceRandom;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = "Wordle is a game where you guess a 5-letter word.")]
 struct Args {
-    // Target word to guess, overrides random mode
-    #[arg(short, long, default_value = "")]
-    word: String,
+    /// Target word to guess, overrides random mode
+    #[arg(short, long)]
+    word: Option<String>,
 
-    // Whether use a random target
+    /// Whether use a random target
     #[arg(short, long, default_value_t = false)]
     random: bool,
 
-    // Whether use difficult mode
-    #[arg(short, long, default_value_t = false)]
+    /// Whether use difficult mode
+    #[arg(short = Some('D'), long, default_value_t = false)]
     difficult: bool,
 }
 
 
 /// The main function for the Wordle game, implement your own logic here
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let is_tty = true; // atty::is(atty::Stream::Stdout);
+    let is_tty = atty::is(atty::Stream::Stdout);
     let args = Args::parse();
-    let target = if args.word.is_empty() {
-        if args.random {
+    let target = match args.word {
+        Some(word) => word,
+        None => if args.random {
             let mut rng = rand::thread_rng();
             let target = FINAL.choose(&mut rng).unwrap();
             target.to_string()
@@ -40,7 +41,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             io::stdin().read_line(&mut target)?;
             target
         }
-    } else { args.word };
+    };
     assert!(FINAL.contains(&target.trim().to_lowercase().as_str()));
     let target = target.trim().to_uppercase();
     let target = target.as_str();
