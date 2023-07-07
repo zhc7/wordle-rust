701b976041246bf22879c6da4a9bd0c130e691de
zhc7 <hc-zhang22@mails.tsinghua.edu.cn>
2023-07-07 14:00:43 +0800

    fix: specify vite target to higher demand to release

--- www/vite.config.js
+++ www/vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import wasm from 'vite-plugin-wasm'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      vue(),
-      wasm(),
-  ],
+    plugins: [
+        vue(),
+        wasm(),
+    ],
+    build: {
+        target: 'esnext'
+    }
 })
